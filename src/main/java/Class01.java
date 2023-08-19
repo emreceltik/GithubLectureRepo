db135b2c201e26e442c4ed67c39261cae5f092e7
Mehmet Emre Celtik <emreceltik@gmail.com>
2023-08-19 20:28:19 +0200

    v3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,24 +4,28 @@ public class Class01 {
         //Versiyon 1 Kodlari
         System.out.println("Version1");
 
-        //Version 2 Kodlari
+        //Versiyon 2 Kodlari
         System.out.println("Version2");
 
-        //git init ile terminalden kaydediyoruz. (repostories -kaydedecegimiz yer- oluşturuyoruz
-        //kod yazdigimiz yer WS (working space)
-//git status statüyü kontrol ettiriyor (guncellenmis kod var ise bize soyluyor)
-//git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani (SA-) Staging Area.
+        //Versiyon 3 Kodlari
+        System.out.println("Version3");
+
+//git init ==>ile terminalden kaydediyoruz. (repostories -kaydedecegimiz yer- oluşturuyoruz
+//kod yazdigimiz yer WS (working space)
+//git status ==>statüyü kontrol ettiriyor (guncellenmis kod var ise bize soyluyor, SA'ya gonderilecek kodlari)
+//git add . ==>ile tüm eksikleri staging area'ya yükler. Geçici alan (SA) Staging Area.
 // yesil olunca gecici alanda, version lama bekliyor
 
-//git commit -m" başlık gelecek buraya "  ----version ekliyoruz (gecici alandan versionlamis oluyoruz, commitliyoruz)
-        // Commit Area da
+//git commit -m" başlık gelecek buraya " ==>version ekliyoruz (gecici alandan versionlamis oluyoruz, commitliyoruz)
+// Commit Area
 
-//git log ile ==> version lari gosterir
-//git log --oneline   ==>tek satirda versionlari gosterir
-//cls ile alan temizleniyor kodlar yani
-//git commit --amend -m""  isim değiştirme (hangi versiyondaysan)
-        // klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
-//git log --oneline ile versiyonları tek satırlar halinde gösteriyor.
+//git log ile ==> version lari ayrintili gosterir (commit de hash kodu yani kimlik bilgisi var, yazar ve tarih de var)
+        //Qq
+//git log --oneline   ==>tek satirda versionlari gosterir (burada da sayilar var. version un kimlik numaralarinin ilk 7 hanesi)
+//cls ==> ile alan temizleniyor kodlar yani
+//git commit --amend -m""  ==> isim değiştirme (hangi versiyondaysan)
+// klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
+//git log --oneline ==> ile versiyonları tek satırlar halinde gösteriyor.
 
     }
 }
