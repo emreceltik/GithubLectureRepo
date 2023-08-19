dd39838af9cf8c6df33e5430f52f2074fda47465
Mehmet Emre Celtik <emreceltik@gmail.com>
2023-08-19 20:14:22 +0200

    v1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,18 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        //Versiyon 1 Kodlari
+        System.out.println("Version1");
+
+        //git init ile terminalden kaydediyoruz. (repostories -kaydedecegimiz yer- oluşturuyoruz
+//git status statüyü kontrol ettiriyor
+//git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani.
+//git commit -m" başlık gelecek buraya "
+//git log ile versiyon görünür bilgiler
+//cls ile alan temizleniyor kodlar yani
+//git commit --amend -m""  isim değiştirme (hangi versiyondaysan)
+        // klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
+//git log --oneline ile versiyonları tek satırlar halinde gösteriyor.
+
+    }
+}
