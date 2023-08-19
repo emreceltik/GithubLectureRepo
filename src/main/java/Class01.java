e35bcf7a514cec1da0756b91c423c50b6725ccde
Mehmet Emre Celtik <emreceltik@gmail.com>
2023-08-19 20:21:07 +0200

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,11 +4,20 @@ public class Class01 {
         //Versiyon 1 Kodlari
         System.out.println("Version1");
 
+        //Version 2 Kodlari
+        System.out.println("Version2");
+
         //git init ile terminalden kaydediyoruz. (repostories -kaydedecegimiz yer- oluşturuyoruz
-//git status statüyü kontrol ettiriyor
-//git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani.
-//git commit -m" başlık gelecek buraya "
-//git log ile versiyon görünür bilgiler
+        //kod yazdigimiz yer WS (working space)
+//git status statüyü kontrol ettiriyor (guncellenmis kod var ise bize soyluyor)
+//git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani (SA-) Staging Area.
+// yesil olunca gecici alanda, version lama bekliyor
+
+//git commit -m" başlık gelecek buraya "  ----version ekliyoruz (gecici alandan versionlamis oluyoruz, commitliyoruz)
+        // Commit Area da
+
+//git log ile ==> version lari gosterir
+//git log --oneline   ==>tek satirda versionlari gosterir
 //cls ile alan temizleniyor kodlar yani
 //git commit --amend -m""  isim değiştirme (hangi versiyondaysan)
         // klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
